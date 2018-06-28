88ae4d751b97fe2fe9b0acf01f882c07a205ea91
Bampi-Cynthia <bam15003@byui.edu>
2018-06-28 11:58:53 -0600

    database

--- index.js
+++ index.js
@@ -10,9 +10,21 @@ const express = require('express');
 const Pool = require('pg').Pool;
 const sql = require('pga-sql');
 
+const db = new Pool({
+connectionString: process.env.DATABASE_URL,
+ssl: true
+})
+
+
+
+
 let app = express();
 app.get('/', function(req, res) {
 	res.end("hello, world!");
+});
+
+app.get('/account', function(req, res){
+	res.end('all accounts');
 });
 
 app.listen(process.env.PORT || 3000, function() {
