8a19926716707f52ad37f523caa40da818d463bb
Bampi-Cynthia <bam15003@byui.edu>
2018-06-28 11:37:53 -0600

    assignment

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const http = require('http');
 
+/*
 http.createServer((req, res) => {
   res.end('Hello, Bom dia!');
 }).listen(process.env.PORT || 3000, () => {
   console.log('Now listening for connections!');
+});
+*/
+const express = require('express');
+const Pool = require('pg').Pool;
+const sql = require('pga-sql');
+
+let app = express();
+app.get('/', function(req, res) {
+	res.end("hello, world!");
+});
+
+app.listen(process.env.PORT || 3000, function() {
+	console.log('Now listening');
 });
